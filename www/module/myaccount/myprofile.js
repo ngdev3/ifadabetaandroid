0d2be5ae0f65ae31c48e2d979ade24ae7b00bac1
sajal <sajal@tekshapers.com>
2018-12-06 18:22:49 +0530

    sajal

--- www/module/myaccount/myprofile.js
+++ www/module/myaccount/myprofile.js
@@ -314,9 +314,38 @@ app.controller('myprofile', function ($scope, $http, $location, $interval, $cook
         })
     }
 
-     $scope.myFunc = function() {  
-      alert();
-      console.log($scope.image)
-      $scope.img =Base64.encode($scope.image);
+    $scope.fileNameChanged = function() {
+        
+         navigator.camera.fileNameChanged(onSuccess, onFail, { quality: 75, targetWidth: 320,
+        targetHeight: 320, destinationType: 0 }); 
+        //destination type was a base64 encoding
+        function onSuccess(imageData) {
+            //preview image on img tag
+            $('#image-preview').attr('src', "data:image/jpeg;base64,"+imageData);
+            //setting scope.lastPhoto 
+            $scope.lastPhoto = dataURItoBlob("data:image/jpeg;base64,"+imageData);
+            console.log($scope.lastPhoto)
+        }
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
     } 
+    function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+     var byteString = atob(dataURI.split(',')[1]);
+     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    
+     var ab = new ArrayBuffer(byteString.length);
+     var ia = new Uint8Array(ab);
+     for (var i = 0; i < byteString.length; i++)
+     {
+        ia[i] = byteString.charCodeAt(i);
+     }
+    
+     var bb = new Blob([ab], { "type": mimeString });
+     return bb; 
+    }
+
+    /* var imagedata= formData.append('photo', $scope.lastPhoto, $scope.publish.name+'.jpg')
+    console.log(imagedata) */
 });
